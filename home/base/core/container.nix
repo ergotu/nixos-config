e6e28b40540afc4eec3a48632833c43e55a1f5f5
Jordi Lambrechts <jordi@intigriti.com>
2024-08-19 08:08:30 +0200

    add plugin to k9s

--- home/base/core/container.nix
+++ home/base/core/container.nix
@@ -17,6 +17,28 @@
   programs = {
     k9s = {
       enable = true;
+      plugin = {
+        plugins = {
+          toggle-helmrelease = {
+            shortCut = "Shift-T";
+            confirm = true;
+            scopes = ["helmreleases"];
+            description = "Suspend or resume a HelmRelease";
+            command = "bash";
+            background = false;
+            args = [
+              "-c"
+              "suspended=$(kubectl --context $CONTEXT get helmreleases -n $NAMESPACE -o=custom-columns=TYPE:.spec.suspend | tail -1);"
+              "verb=$([ $suspended = \"true\" ] && echo \"resume\" || echo \"suspend\");"
+              "flux"
+              "$verb helmrelease"
+              "--context $CONTEXT"
+              "-n $NAMESPACE $NAME"
+              "| less -K"
+            ];
+          };
+        };
+      };
     };
   };
 }
