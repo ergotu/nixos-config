2a8fafe84b89993cb5ffb3a744c057d6fd8bf50f
Jordi Lambrechts <jordi@intigriti.com>
2024-08-21 13:26:07 +0200

    add kernel params for k3s cluster

--- hosts/k3s-master-0/default.nix
+++ hosts/k3s-master-0/default.nix
@@ -40,6 +40,28 @@ in {
   };
 
   services.qemuGuest.enable = true;
+  boot.kernel.sysctl = {
+    # --- filesystem --- #
+    # increase the limits to avoid running out of inotify watches
+    "fs.inotify.max_user_watches" = 524288;
+    "fs.inotify.max_user_instances" = 1024;
+
+    # --- network --- #
+    "net.bridge.bridge-nf-call-iptables" = 1;
+    "net.core.somaxconn" = 32768;
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv4.conf.all.forwarding" = 1;
+    "net.ipv4.neigh.default.gc_thresh1" = 4096;
+    "net.ipv4.neigh.default.gc_thresh2" = 6144;
+    "net.ipv4.neigh.default.gc_thresh3" = 8192;
+    "net.ipv4.neigh.default.gc_interval" = 60;
+    "net.ipv4.neigh.default.gc_stale_time" = 120;
+
+    "net.ipv6.conf.all.disable_ipv6" = 1; # disable ipv6
+
+    # --- memory --- #
+    "vm.swappiness" = 0; # don't swap unless absolutely necessary
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
