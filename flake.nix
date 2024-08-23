4d716b87725c499ddd33f66f2c4e27476099297a
Jordi Lambrechts <jordi@intigriti.com>
2024-08-23 11:47:15 +0200

    feat: add default trusted caches

--- flake.nix
+++ flake.nix
@@ -6,14 +6,12 @@
   nixConfig = {
     # substituers will be appended to the default substituters when fetching packages
     extra-substituters = [
-      "https://nix-community.cachix.org"
       "https://anyrun.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-gaming.cachix.org"
       # "https://nixpkgs-wayland.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
--- modules/base.nix
+++ modules/base.nix
@@ -77,10 +77,16 @@
 
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org?priority=41" # this is a useful public cache!
+      "https://numtide.cachix.org?priority=42" # this is also a useful public cache!
+      "https://attic.ergotu.com/system?priority=43"
     ];
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "system:S698HWAIjLxOartA9FtdP/KZA3KEOjmhXWtgMVTduIE="
     ];
     builders-use-substitutes = true;
   };
