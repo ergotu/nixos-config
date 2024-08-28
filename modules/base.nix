b5f33a2625e46008b1cf71662ffa230f0331c109
Jordi Lambrechts <jordi@intigriti.com>
2024-08-28 07:26:32 +0200

    fix: fix the attic cache key

--- modules/base.nix
+++ modules/base.nix
@@ -87,7 +87,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "system:S698HWAIjLxOartA9FtdP/KZA3KEOjmhXWtgMVTduIE="
+      "system:tqRuvHZjHCGH0dWDXShIRPlocnrtZGPbXIhv7AxC+0s="
     ];
     builders-use-substitutes = true;
   };
