4ad0ec9eeb84b83fd725a40c2e03789eaf3c1e08
Jordi Lambrechts <jordi@intigriti.com>
2024-09-09 21:26:58 +0200

    chore: set configurationLimit for grub machines

--- modules/nixos/base/core.nix
+++ modules/nixos/base/core.nix
@@ -3,11 +3,16 @@
   lib,
   ...
 }: {
-  boot.loader.systemd-boot = {
-    # we use Git for version control, so we don't need to keep too many generations.
-    configurationLimit = lib.mkDefault 10;
-    # pick the highest resolution for systemd-boot's console.
-    consoleMode = lib.mkDefault "max";
+  boot.loader = {
+    systemd-boot = {
+      # we use Git for version control, so we don't need to keep too many generations.
+      configurationLimit = lib.mkDefault 10;
+      # pick the highest resolution for systemd-boot's console.
+      consoleMode = lib.mkDefault "max";
+    };
+    grub = {
+      configurationLimit = lib.mkDefault 10;
+    };
   };
 
   # for power management
