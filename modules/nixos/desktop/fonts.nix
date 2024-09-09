44e33e18528201d86039dc7b71c7089a5ce8c6b5
Jordi Lambrechts <jordi@intigriti.com>
2024-09-09 11:55:01 +0200

    feat: add font config for desktop machines

---
+++ modules/nixos/desktop/fonts.nix
@@ -0,0 +1,54 @@
+{pkgs, ...}: {
+  fonts = {
+    enableDefaultPackages = false;
+    fontDir.enable = true;
+
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
+      noto-fonts-emoji
+
+      fira-code
+      iosevka
+      noto-fonts-emoji
+      monaspace
+      jetbrains-mono
+
+      source-sans
+      source-serif
+
+      (nerdfonts.override {
+        fonts = [
+          # symbols only
+          "NerdFontsSymbolsOnly"
+
+          # characters
+          "FiraCode"
+          "Iosevka"
+          "Monaspace"
+          "JetBrainsMono"
+        ];
+      })
+    ];
+    fontconfig.defaultFonts = {
+      serif = ["Source Serif SC" "Noto Color Emoji"];
+      sansSerif = ["Source Sans SC" "Noto Color Emoji"];
+      monospace = ["Fira Code Mono" "Noto Color Emoji"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
+
+  services.kmscon = {
+    enable = true;
+    fonts = [
+      {
+        name = "Source Code Pro";
+        package = pkgs.source-code-pro;
+      }
+    ];
+    extraOptions = "--term xterm-256color";
+    extraConfig = "font-size=14";
+    hwRender = true;
+  };
+}
