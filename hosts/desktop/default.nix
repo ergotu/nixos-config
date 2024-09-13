a482e32de359984fd7d6fbdf42dd41efe57ab933
Jordi Lambrechts <jordi@intigriti.com>
2024-09-13 08:16:40 +0200

    chore: update pre-commit checks

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,9 +1,5 @@
-{
-  lib,
-  disko,
-  ...
-}: let
-  hostName = "desktop";
+{ lib, disko, ... }:
+let hostName = "desktop";
 in {
   imports = [
     ./hardware-configuration.nix
@@ -18,12 +14,13 @@ in {
   };
 
   # For Nvidia GPU
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
 
+    open = false;
     # required by most wayland compositors!
     modesetting.enable = true;
     powerManagement.enable = true;
--- outputs/default.nix
+++ outputs/default.nix
@@ -1,13 +1,8 @@
-{
-  self,
-  nixpkgs,
-  pre-commit-hooks,
-  nvimdots,
-  ...
-} @ inputs: let
+{ self, nixpkgs, pre-commit-hooks, nvimdots, ... }@inputs:
+let
   inherit (inputs.nixpkgs) lib;
-  mylib = import ../lib {inherit lib;};
-  myvars = import ../vars {inherit lib;};
+  mylib = import ../lib { inherit lib; };
+  myvars = import ../vars { inherit lib; };
 
   opts = {
     border = "rounded";
@@ -17,8 +12,7 @@
   # Add my custom lib, vars, nixpkgs instance, and all the inputs to specialArgs,
   # so that I can use them in all my nixos/home-manager/darwin modules.
   genSpecialArgs = system:
-    inputs
-    // {
+    inputs // {
       inherit mylib myvars;
       inherit (mylib) mkKey;
 
@@ -26,7 +20,8 @@
 
       # use unstable branch for some packages to get the latest updates
       pkgs-unstable = import inputs.nixpkgs-unstable {
-        inherit system; # refer the `system` parameter form outer scope recursively
+        inherit
+          system; # refer the `system` parameter form outer scope recursively
         # To use chrome, we need to allow the installation of non-free software
         config.allowUnfree = true;
       };
@@ -39,19 +34,22 @@
         config.allowUnfree = true;
       };
 
-      secrets = builtins.fromJSON (builtins.readFile "${self}/secrets/secrets.json");
+      secrets =
+        builtins.fromJSON (builtins.readFile "${self}/secrets/secrets.json");
     };
 
   # This is the args for all the haumea modules in this folder.
-  args = {inherit inputs lib mylib myvars genSpecialArgs;};
+  args = { inherit inputs lib mylib myvars genSpecialArgs; };
 
   # modules for each supported system
   nixosSystems = {
-    x86_64-linux = import ./x86_64-linux (args // {system = "x86_64-linux";});
-    aarch64-linux = import ./aarch64-linux (args // {system = "aarch64-linux";});
+    x86_64-linux = import ./x86_64-linux (args // { system = "x86_64-linux"; });
+    aarch64-linux =
+      import ./aarch64-linux (args // { system = "aarch64-linux"; });
   };
   darwinSystems = {
-    aarch64-darwin = import ./aarch64-darwin (args // {system = "aarch64-darwin";});
+    aarch64-darwin =
+      import ./aarch64-darwin (args // { system = "aarch64-darwin"; });
   };
   allSystems = nixosSystems // darwinSystems;
   allSystemNames = builtins.attrNames allSystems;
@@ -65,83 +63,96 @@
   homeSystems = import ./homemanager args;
 in {
   # Add attribute sets into outputs, for debugging
-  debugAttrs = {inherit nixosSystems darwinSystems allSystems allSystemNames homeSystems;};
+  debugAttrs = {
+    inherit nixosSystems darwinSystems allSystems allSystemNames homeSystems;
+  };
 
   homeConfigurations = homeSystems.homeConfigurations;
 
   # NixOS Hosts
-  nixosConfigurations =
-    lib.attrsets.mergeAttrsList (map (it: it.nixosConfigurations or {}) nixosSystemValues);
+  nixosConfigurations = lib.attrsets.mergeAttrsList
+    (map (it: it.nixosConfigurations or { }) nixosSystemValues);
 
   # Colmena - remote deployment via SSH
-  colmena =
-    {
-      meta =
-        (
-          let
-            system = "x86_64-linux";
-          in {
-            # colmena's default nixpkgs & specialArgs
-            nixpkgs = import nixpkgs {inherit system;};
-            specialArgs = genSpecialArgs system;
-          }
-        )
-        // {
-          # per-node nixpkgs & specialArgs
-          nodeNixpkgs = lib.attrsets.mergeAttrsList (map (it: it.colmenaMeta.nodeNixpkgs or {}) nixosSystemValues);
-          nodeSpecialArgs = lib.attrsets.mergeAttrsList (map (it: it.colmenaMeta.nodeSpecialArgs or {}) nixosSystemValues);
-        };
-    }
-    // lib.attrsets.mergeAttrsList (map (it: it.colmena or {}) nixosSystemValues);
+  colmena = {
+    meta = (let system = "x86_64-linux";
+    in {
+      # colmena's default nixpkgs & specialArgs
+      nixpkgs = import nixpkgs { inherit system; };
+      specialArgs = genSpecialArgs system;
+    }) // {
+      # per-node nixpkgs & specialArgs
+      nodeNixpkgs = lib.attrsets.mergeAttrsList
+        (map (it: it.colmenaMeta.nodeNixpkgs or { }) nixosSystemValues);
+      nodeSpecialArgs = lib.attrsets.mergeAttrsList
+        (map (it: it.colmenaMeta.nodeSpecialArgs or { }) nixosSystemValues);
+    };
+  } // lib.attrsets.mergeAttrsList
+    (map (it: it.colmena or { }) nixosSystemValues);
 
   # macOS Hosts
-  darwinConfigurations =
-    lib.attrsets.mergeAttrsList (map (it: it.darwinConfigurations or {}) darwinSystemValues);
+  darwinConfigurations = lib.attrsets.mergeAttrsList
+    (map (it: it.darwinConfigurations or { }) darwinSystemValues);
 
   # Packages
-  packages = forAllSystems (
-    system: allSystems.${system}.packages or {}
-  );
+  packages = forAllSystems (system: allSystems.${system}.packages or { });
 
   # Eval Tests for all NixOS & darwin systems.
-  evalTests = lib.lists.all (it: it.evalTests == {}) allSystemValues;
+  evalTests = lib.lists.all (it: it.evalTests == { }) allSystemValues;
 
-  checks = forAllSystems (
-    system: {
+  checks = forAllSystems (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in {
       # eval-tests per system
       # eval-tests = allSystems.${system}.evalTests == {};
 
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = mylib.relativeToRoot ".";
+      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        default_stages = [ "pre-commit" ];
         hooks = {
           commitizen.enable = true;
-          alejandra.enable = true; # formatter
-          # Source code spell checker
-          typos = {
+          check-added-large-files.enable = true;
+          check-case-conflicts.enable = true;
+          check-executables-have-shebangs.enable = true;
+          check-shebang-scripts-are-executable.enable = true;
+          check-merge-conflicts.enable = true;
+          detect-private-keys.enable = true;
+          fix-byte-order-marker.enable = true;
+          mixed-line-endings.enable = true;
+          trim-trailing-whitespace.enable = true;
+
+          forbid-submodules = {
             enable = true;
-            settings = {
-              write = true; # Automatically fix typos
-              configPath = "./.typos.toml"; # relative to the flake root
-            };
+            name = "forbid submodules";
+            description = "forbids any submodules in the repository";
+            language = "fail";
+            entry = "submodules are not allowed in this repository:";
+            types = [ "directory" ];
           };
-          prettier = {
+
+          destroyed-symlinks = {
             enable = true;
-            settings = {
-              write = true; # Automatically format files
-              configPath = "./.prettierrc.yaml"; # relative to the flake root
-            };
+            name = "destroyed-symlinks";
+            description =
+              "detects symlinks which are changed to regular files with a content of a path which that symlink was pointing to.";
+            package = inputs.pre-commit-hooks.checks.${system}.pre-commit-hooks;
+            entry = "${
+                inputs.pre-commit-hooks.checks.${system}.pre-commit-hooks
+              }/bin/destroyed-symlinks";
+            types = [ "symlink" ];
           };
-          # deadnix.enable = true; # detect unused variable bindings in `*.nix`
-          # statix.enable = true; # lints and suggestions for Nix code(auto suggestions)
+
+          alejandra.enable = true;
+          shfmt.enable = true;
+
+          end-of-file-fixer.enable = true;
         };
       };
-    }
-  );
+    });
 
   # Development Shells
-  devShells = forAllSystems (
-    system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  devShells = forAllSystems (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
     in {
       default = pkgs.mkShell {
         packages = with pkgs; [
@@ -165,12 +176,10 @@ in {
           ${self.checks.${system}.pre-commit-check.shellHook}
         '';
       };
-    }
-  );
+    });
 
   # Format the nix code in this flake
   formatter = forAllSystems (
     # alejandra is a nix formatter with a beautiful output
-    system: nixpkgs.legacyPackages.${system}.alejandra
-  );
+    system: nixpkgs.legacyPackages.${system}.alejandra);
 }
